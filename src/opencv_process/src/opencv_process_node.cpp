1d255e3298e04440fc77e715fe84662dcc97a18c
Chshy <szclsxycs@163.com>
2021-10-26 18:35:54 +0800

    找竖杆

--- src/opencv_process/src/opencv_process_node.cpp
+++ src/opencv_process/src/opencv_process_node.cpp
@@ -43,16 +43,27 @@ int main(int argc, char **argv)
         if (d435.new_img_pair_come())
         {
             cv::Mat RGB_src = d435.getCam(D435_RGB_Cam);
-            cv::Mat Dep_src = d435.getCam(D435_Depth_Cam, 50, 3000);
+            cv::Mat Dep_src = d435.getCam(D435_Depth_Cam, 50, 1000);
             cv::Mat Dep_display = d435.getDepDisplay(Dep_src);
             cv::Mat Binding;
             cv::addWeighted(RGB_src, 0.5, Dep_display, 0.5, 3, Binding);
             cv::imshow("binding", Binding);
 
-            cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9)));
-            cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 27)));
+            // cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9)));
+            // cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 27)));
+
+            // cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9)));
+            // cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 27)));
+            
             
 
+            // cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9)));
+            //先用（9，9）的闭运算清除深度图中的空洞
+            cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9)));
+            //然后用非对称卷积核找竖直方向的countor
+            //可视场地内容适当缩短长边
+            cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 159)));
+            cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 159)));
 
 
             Dep_display = d435.getDepDisplay(Dep_src);
