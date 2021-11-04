44ea2b88195a07b74432cb51839083c58bbba439
Chshy <szclsxycs@163.com>
2021-11-04 14:19:42 +0800

    删除多余函数

--- src/opencv_process/include/opencv_process/dep_img_proc.hpp
+++ src/opencv_process/include/opencv_process/dep_img_proc.hpp
@@ -1,42 +1,16 @@
 #pragma once
 #include <opencv2/opencv.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/core/base.hpp>
-#include <opencv2/core/core.hpp>
 
-cv::Mat get_display_img(cv::Mat dep_32fc1, float min_distance, float max_distance) //mm
+void FindVertivalBar(cv::Mat dep_img, std::vector<cv::Rect2i> &reslt)
 {
-    //valve_cover为二值化图像，有效范围内的为255，外的为0，用于标记可信数据(其实是在显示图像上把没用的数据覆盖掉)
-    cv::Mat valve_cover(dep_32fc1.rows, dep_32fc1.cols, CV_8UC1);
-    inRange(dep_32fc1, min_distance, max_distance, valve_cover);
+    //先用（9，9）的闭运算清除深度图中的空洞
+    cv::morphologyEx(dep_img, dep_img, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9)));
+    //然后用非对称卷积核找竖直方向的countor
+    //可视场地内容适当缩短长边
+    cv::morphologyEx(dep_img, dep_img, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 159)));
+    cv::morphologyEx(dep_img, dep_img, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 159)));
 
-    //用于数据处理
-    //dep_16uc1是16bit单通道图像，与dep_32fc1之间等值(因为dep_32fc1只出现整数且最大值为65535)
-    cv::Mat dep_16uc1(dep_32fc1.rows, dep_32fc1.cols, CV_16UC1);
-    dep_32fc1.convertTo(dep_16uc1, CV_16UC1);
-
-    //去除不信任数据
-    threshold(dep_32fc1,dep_32fc1,min_distance,0xFFFF,CV_THRESH_TOZERO);//将比最小信任距离小的点归零
-    threshold(dep_32fc1,dep_32fc1,max_distance,0xFFFF,CV_THRESH_TOZERO_INV);//将比最大信任距离大的点归零
     
-    //显示用图像
-    cv::Mat Dep_HSV(dep_32fc1.rows, dep_32fc1.cols, CV_8UC3);
-    //dep_8uc1为8bit单通道图像，用于显示
-    cv::Mat dep_8uc1(dep_32fc1.rows, dep_32fc1.cols, CV_8UC1);
-    //转换格式，将最大信任值以内的数值（包括小于最小信任值的）归一化到120（仍然可能出现大于120的值）
-    dep_32fc1.convertTo(dep_8uc1, CV_8UC1, 120.0 / max_distance);
-
-    //用于显示的部分
-    std::vector<cv::Mat> channels_hsv;
-    cv::Mat blank_ch, full_ch;
-    blank_ch = cv::Mat::zeros(cv::Size(dep_8uc1.cols, dep_8uc1.rows), CV_8UC1);
-    full_ch = cv::Mat(cv::Size(dep_8uc1.cols, dep_8uc1.rows), CV_8UC1, 255);
-    //合成HSV图像
-    channels_hsv.push_back(dep_8uc1);
-    channels_hsv.push_back(full_ch);
-    channels_hsv.push_back(valve_cover);
-    cv::merge(channels_hsv, Dep_HSV);
-    cv::cvtColor(Dep_HSV, Dep_HSV, CV_HSV2BGR);
-    return Dep_HSV;
+    cv::Rect2i();
+    
 }
--- src/opencv_process/src/opencv_process_node.cpp
+++ src/opencv_process/src/opencv_process_node.cpp
@@ -1,19 +1,8 @@
 #include <cv_bridge/cv_bridge.h>
-
 #include <opencv2/opencv.hpp>
-
-// #include <opencv2/highgui.hpp>
-
-// #include <opencv2/imgproc.hpp>
-
-// #include <opencv2/core/base.hpp>
-
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
-
 #include <opencv_process/d435_subscriber.hpp>
-
-// #include <opencv_process/dep_img_proc.hpp>
 
 int main(int argc, char **argv)
 {
@@ -26,20 +15,6 @@ int main(int argc, char **argv)
     {
         ros::spinOnce();
 
-        // if (d435.RGB_sub.new_img_come())
-        // {
-        //     // cv::imshow("rgb", d435.RGB_sub.get_cv_img());
-        //     cv::imshow("rgb", d435.getCam(D435_RGB_Cam));
-        //     // std::cout << "rgb = " << d435.RGB_sub.get_cv_img().type() << std::endl;
-        // }
-
-        // if (d435.Depth_sub.new_img_come())
-        // {
-        //     cv::Mat tmp = d435.getCam(D435_Depth_Cam, 50, 3000, true);
-        //     // cv::morphologyEx(tmp, tmp, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3)));
-        //     // cv::GaussianBlur(tmp,tmp,cv::Size(5,5),2,2);
-        //     cv::imshow("Depth", d435.getDepDisplay(tmp));
-        // }
         if (d435.new_img_pair_come())
         {
             cv::Mat RGB_src = d435.getCam(D435_RGB_Cam);
@@ -48,27 +23,15 @@ int main(int argc, char **argv)
             cv::Mat Binding;
             cv::addWeighted(RGB_src, 0.5, Dep_display, 0.5, 3, Binding);
             cv::imshow("binding", Binding);
-
-            // cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9)));
-            // cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 27)));
-
+            // //先用（9，9）的闭运算清除深度图中的空洞
             // cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9)));
-            // cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 27)));
-            
-            
-
-            // cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9)));
-            //先用（9，9）的闭运算清除深度图中的空洞
-            cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9)));
-            //然后用非对称卷积核找竖直方向的countor
-            //可视场地内容适当缩短长边
-            cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 159)));
-            cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 159)));
-
-
-            Dep_display = d435.getDepDisplay(Dep_src);
-            cv::addWeighted(RGB_src, 0.5, Dep_display, 0.5, 3, Binding);
-            cv::imshow("process", Binding);
+            // //然后用非对称卷积核找竖直方向的countor
+            // //可视场地内容适当缩短长边
+            // cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 159)));
+            // cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 159)));
+            // Dep_display = d435.getDepDisplay(Dep_src);
+            // cv::addWeighted(RGB_src, 0.5, Dep_display, 0.5, 3, Binding);
+            // cv::imshow("process", Binding);
         }
 
         cv::waitKey(1);
