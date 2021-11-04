c3e0a55ccef48e538fb8415fb813a3cb5d5a7d1e
Chshy <szclsxycs@163.com>
2021-11-04 14:29:29 +0800

    usb_cam

--- src/opencv_process/src/opencv_process_node.cpp
+++ src/opencv_process/src/opencv_process_node.cpp
@@ -4,36 +4,23 @@
 #include <sensor_msgs/Image.h>
 #include <opencv_process/d435_subscriber.hpp>
 
+#include <opencv_process/image_converter.hpp>
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "opencv_process_node");
     ros::NodeHandle nh;
 
-    d435_subscriber d435(nh);
+    image_converter usbcam(nh, "/usb_cam/image_raw", 10, sensor_msgs::image_encodings::BGR8);
 
     while (1)
     {
         ros::spinOnce();
-
-        if (d435.new_img_pair_come())
+        if (usbcam.new_img_come())
         {
-            cv::Mat RGB_src = d435.getCam(D435_RGB_Cam);
-            cv::Mat Dep_src = d435.getCam(D435_Depth_Cam, 50, 1000);
-            cv::Mat Dep_display = d435.getDepDisplay(Dep_src);
-            cv::Mat Binding;
-            cv::addWeighted(RGB_src, 0.5, Dep_display, 0.5, 3, Binding);
-            cv::imshow("binding", Binding);
-            // //先用（9，9）的闭运算清除深度图中的空洞
-            // cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9)));
-            // //然后用非对称卷积核找竖直方向的countor
-            // //可视场地内容适当缩短长边
-            // cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 159)));
-            // cv::morphologyEx(Dep_src, Dep_src, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 159)));
-            // Dep_display = d435.getDepDisplay(Dep_src);
-            // cv::addWeighted(RGB_src, 0.5, Dep_display, 0.5, 3, Binding);
-            // cv::imshow("process", Binding);
+            cv::Mat src = usbcam.get_cv_img();
+            cv::imshow("src", src);
         }
-
         cv::waitKey(1);
     }
 
